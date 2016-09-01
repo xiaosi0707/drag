ace5be7442ce7b7e651ee61f6dc56bf7a47c91cf
xiaosi0707 <583514368@qq.com>
2016-09-01 22:53:25 +0800

    版本1.0

--- drag.js
+++ drag.js
@@ -1,16 +1,18 @@
+function drag(id) {
+	var obj = document.getElementById(id);
+	var disX = 0;
+	var disY = 0;
+	obj.onmousedown = function(ev) {
+		disX = ev.pageX - obj.offsetLeft;
+		disY = ev.pageY - obj.offsetTop;
+		document.onmousemove = function(ev) {
 
-function drag(id){
-	var obj=document.getElementById(id);
-	var disX=0;
-	var disY=0;
-	obj.onmousedown=function(ev){
-		disX=ev.pageX-obj.offsetLeft;
-		disY=ev.pageY.Object.offsetTop;
-		document.onmousemove=function(ev){
+			obj.style.left = ev.pageX - disX+'px';
+			obj.style.top = ev.pageY - disY+'px';
 
-			obj.style.left=ev.pageX-disX;
-			obj.style.top=ev.pageY-disY;
-
+		}
+		document.onmouseup = function() {
+			document.onmousemove = document.onmouseup = null;
 		}
 	}
 }
